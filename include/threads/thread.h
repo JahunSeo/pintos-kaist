f9e81422d0f1b83662eeec94fefda3f73e13aaa1
JahunSeo <wkgjs0809@gmail.com>
2021-09-29 14:28:30 +0000

    [johnny] thread.h updated with awake related functions

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -134,6 +134,12 @@ const char *thread_name (void);
 void thread_exit (void) NO_RETURN;
 void thread_yield (void);
 
+void thread_sleep(int64_t ticks);
+void thread_awake(int64_t ticks);
+
+int64_t get_next_tick_to_awake(void);
+void update_next_tick_to_awake(int64_t ticks);
+
 int thread_get_priority (void);
 void thread_set_priority (int);
 
