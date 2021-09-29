a845cfcbbf30a32df824a5d270bb2b950efb5b01
JahunSeo <wkgjs0809@gmail.com>
2021-09-29 14:03:51 +0000

    [johnny] add awake related function & thread_sleep

--- threads/thread.c
+++ threads/thread.c
@@ -316,6 +316,38 @@ thread_yield (void) {
 	intr_set_level (old_level);
 }
 
+/* 가장 먼저 awake해야 하는 thread가 awake되어야 하는 시각을 업데이트 (setter) */
+void update_next_tick_to_awake(int64_t ticks) { 
+	// 질문: next_tick_to_awake를 초기 설정을 하지 않아도 되려나?
+	next_tick_to_awake = (next_tick_to_awake > ticks) ? ticks : next_tick_to_awake;
+}
+
+/* 가장 먼저 awake해야 하는 thread가 awake되어야 하는 시각을 반환 (getter) */
+int64_t get_next_tick_to_awake(void) {
+	return next_tick_to_awake;
+}
+
+/* thread를 ticks까지 재우는 함수 */
+void thread_sleep(int64_t ticks) {
+	struct thread *cur;
+	// interrupt를 막고, 이전 interrupt 상태를 저장함
+	enum intr_level old_level;
+	old_level = intr_disable();
+
+	// 현재 thread를 잠재우기 위해 가져옴 (이 때, idle thread는 sleep되지 않아야 함)
+	cur = thread_current();
+	ASSERT(cur != idle_thread);
+	// awake 함수가 실행될 시점 tick을 update
+	update_next_tick_to_awake(cur->wakeup_tick = ticks);
+	// 현재 thread를 sleep_list에 삽입함
+	list_push_back(&sleep_list, &cur->elem);
+	// 현재 thread의 상태를 block으로 변경 (scheduling까지 진행)
+	thread_block();
+
+	// interrupt 가능 여부를 이전 상태로 되돌림
+	intr_set_level(old_level);
+}
+
 /* Sets the current thread's priority to NEW_PRIORITY. */
 void
 thread_set_priority (int new_priority) {
