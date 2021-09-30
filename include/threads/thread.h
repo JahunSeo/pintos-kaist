b369889624b0343f92ba1ffcf51ca95cb4249e77
JahunSeo <wkgjs0809@gmail.com>
2021-09-30 13:46:00 +0000

    [johnny] update thread.h with compare_priority & test_max_priority

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -143,6 +143,9 @@ void update_next_tick_to_awake(int64_t ticks);
 int thread_get_priority (void);
 void thread_set_priority (int);
 
+bool thread_compare_priority(const struct list_elem *a, const struct list_elem *b, void *aux UNUSED);
+void test_max_priority (void)
+
 int thread_get_nice (void);
 void thread_set_nice (int);
 int thread_get_recent_cpu (void);
