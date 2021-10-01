5485a85bb9adbf7a86b3a618d650481ab0f79a6a
root <root@DESKTOP-J6J63OL.localdomain>
2021-10-02 00:50:07 +0900

    [kjy] priority condvar, preempt develop

--- include/threads/synch.h
+++ include/threads/synch.h
@@ -15,7 +15,7 @@ void sema_down (struct semaphore *);
 bool sema_try_down (struct semaphore *);
 void sema_up (struct semaphore *);
 void sema_self_test (void);
-
+bool sema_compare_priority(const struct list_elem*, const struct list_elem *, void *);
 /* Lock. */
 struct lock {
 	struct thread *holder;      /* Thread holding lock (for debugging). */
