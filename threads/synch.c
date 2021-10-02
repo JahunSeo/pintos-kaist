9c5d200310ab1f47fd1b376bbba521fa6b6bc516
JahunSeo <wkgjs0809@gmail.com>
2021-10-02 13:05:17 +0000

    [johnny] update lock_release

--- threads/synch.c
+++ threads/synch.c
@@ -243,6 +243,9 @@ lock_release (struct lock *lock) {
 	ASSERT (lock != NULL);
 	ASSERT (lock_held_by_current_thread (lock));
 
+	remove_with_lock (lock);
+	refresh_priority ();
+
 	lock->holder = NULL;
 	sema_up (&lock->semaphore);
 }
