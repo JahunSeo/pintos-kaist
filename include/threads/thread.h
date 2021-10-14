8bdf7e9d34f0cdfeeba5cc37d984207634dceb5e
JahunSeo <wkgjs0809@gmail.com>
2021-10-14 01:07:18 +0000

    [johnny] edit FDT_PAGE_CNT & FDT_ENTRY_MAX

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -31,8 +31,11 @@ typedef int tid_t;
 #define PRI_MAX 63                      /* Highest priority. */
 
 /* file descriptor related */
-#define FDT_ENTRY_MAX 64
-#define FDT_PAGE_CNT (FDT_ENTRY_MAX + (PGSIZE - 1)) / (PGSIZE)
+// #define FDT_ENTRY_MAX 64
+// #define FDT_PAGE_CNT (FDT_ENTRY_MAX + (PGSIZE - 1)) / (PGSIZE)
+
+#define FDT_PAGE_CNT 1				
+#define FDT_ENTRY_MAX FDT_PAGES *(1 << 9)    
 
 /* A kernel thread or user process.
  *
