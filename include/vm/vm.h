bbb3b87e364a54c6115951b0454f1d70145073c3
JahunSeo <wkgjs0809@gmail.com>
2021-10-20 11:31:14 +0000

    [johnny] in vm.h, update supplemental_page_table to add data structure

--- include/vm/vm.h
+++ include/vm/vm.h
@@ -85,6 +85,7 @@ struct page_operations {
  * We don't want to force you to obey any specific design for this struct.
  * All designs up to you for this. */
 struct supplemental_page_table {
+	struct hash page_table; // spt의 자료 구조를 hash table로 결정 
 };
 
 #include "threads/thread.h"
