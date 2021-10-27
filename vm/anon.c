0ea8076c54881e09dafdeef1b1e8fd15d1110676
JahunSeo <wkgjs0809@gmail.com>
2021-10-27 14:07:06 +0000

    [johnny] update vm_anon_init()

--- vm/anon.c
+++ vm/anon.c
@@ -3,6 +3,11 @@
 #include "vm/vm.h"
 #include "devices/disk.h"
 
+// ADD
+#define CEILING(x, y) (((x) + (y) - 1) / (y))
+// SECTORS_PER_PAGE: 한 PAGE를 수용하는데 필요한 disk sector의 수 (4096 bytes // 512 bytes)
+#define SECTORS_PER_PAGE CEILING(PGSIZE, DISK_SECTOR_SIZE)
+
 /* DO NOT MODIFY BELOW LINE */
 static struct disk *swap_disk;
 static bool anon_swap_in (struct page *page, void *kva);
@@ -25,11 +30,26 @@ static const struct page_operations stack_ops = {
 	.type = VM_ANON | VM_MARKER_0,
 };
 
+/* swap table 
+  - 특정 slot이 사용중인지 여부를 체크하는데, bitmap table이 유리
+  - anon.c에서만 접근할 것이므로 static 으로 설정
+*/
+static struct bitmap *swap_table;
+
 /* Initialize the data for anonymous pages */
 void
 vm_anon_init (void) {
 	/* TODO: Set up the swap_disk. */
-	swap_disk = NULL;
+	// disk에서 swap을 위한 영역 받아오기: swap을 위해서는 1, 1로 설정
+	swap_disk = dist_get(1, 1); 
+	// 섹터 단위로 표현된 swap disk의 크기
+	//  - the size of disk D, measured in DISK_SECTOR_SIZE-byte sectors. 
+	disk_sector_t disk_capacity = disk_size(swap_disk);
+	// disk 내에 몇 개의 페이지를 수용 가능한지 계산
+	//  - SECTORS_PER_PAGE : 한 page를 수용하는데 필요한 sector의 수 (4096byte // 512bytes)
+	size_t max_slot = disk_capacity / SECTORS_PER_PAGE;
+	// swap table 초기화
+	swap_table = bitmap_create(max_slot);
 }
 
 /* Initialize the file mapping */
